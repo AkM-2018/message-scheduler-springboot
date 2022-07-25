5a548ccedd55c3006e53228f4ac5cf9d50c7371d
AkM-2018 <amanoj1234567890@gmail.com>
2022-07-25 14:22:15 +0530

    Removed some imports

--- src/test/java/com/akm/messagescheduler/service/MessageScheduleSendServiceTest.java
+++ src/test/java/com/akm/messagescheduler/service/MessageScheduleSendServiceTest.java
@@ -11,9 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
